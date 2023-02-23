45b4b8d8a163802fe73e915b5ebf1b0496fb6feb
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-02-23 00:54:36 -0500

    Added @AfterEach method to DatabaseConnectionTest class

--- src/test/java/repository/DatabaseConnectionTest.java
+++ src/test/java/repository/DatabaseConnectionTest.java
@@ -1,50 +1,28 @@
 package repository;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
-import java.util.Properties;
+import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseConnectionTest {
-
-    @Test
-    void testReadConfigPropertiesFile() {
-        //Create an input stream to read the properties file
-        String expectedUrl = null;
-        String expectedUser = null;
-        String expectedPassword = null;
-
-        try (InputStream input = Files.newInputStream(
-                Paths.get("src/main/resources/config.properties")
-        )) {
-
-            Properties prop = new Properties();
-
-            // Load the properties file to the properties object
-            prop.load(input);
-
-            // Get the property value and print it out
-            expectedUrl = prop.getProperty("mysql.db.url");
-            expectedUser = prop.getProperty("mysql.db.user");
-            expectedPassword = prop.getProperty("mysql.db.password");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        assertEquals(expectedUrl, "jdbc:mysql://localhost:3307/test");
-        assertEquals(expectedUser, "test_user");
-        assertEquals(expectedPassword, "secret");
-    }
+    Connection connection;
     @Test
     void testGetDatabaseConnection() {
-        Connection connection = DatabaseConnection.getConnection();
+        connection = DatabaseConnection.getConnection();
         assertNotNull(connection);
+    }
+
+    @AfterEach
+    void closeConnection() {
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            System.out.println("Error closing the database connection " + e.getMessage());
+        }
     }
 }
